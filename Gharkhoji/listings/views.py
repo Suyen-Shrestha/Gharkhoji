5c1fdd300d55bcd5048d7d1d6aeb8bad8933dcfc
Suyen-Shrestha <suyencrestha@gmail.com>
2020-01-04 23:36:11 +0545

    Single listing detail page completed.

--- Gharkhoji/listings/views.py
+++ Gharkhoji/listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
@@ -17,8 +17,14 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, id=listing_id)
+
+
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
