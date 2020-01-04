121485f741c9fa546893dd7cd5011dc7dcf91b2e
Suyen-Shrestha <suyencrestha@gmail.com>
2020-01-04 16:21:14 +0545

    Frontend almost completed.

--- Gharkhoji/Gharkhoji/settings.py
+++ Gharkhoji/Gharkhoji/settings.py
@@ -121,5 +121,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Gharkhoji/static')
+    os.path.join(BASE_DIR, 'Gharkhoji/static'),
+
 ]
