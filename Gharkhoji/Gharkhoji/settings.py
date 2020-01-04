21b84ba680cc689472d5320da6bcdc2663104777
Suyen-Shrestha <suyencrestha@gmail.com>
2020-01-04 20:18:25 +0545

    Listings displayed on the frontend.

--- Gharkhoji/Gharkhoji/settings.py
+++ Gharkhoji/Gharkhoji/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'pages',
     'listings',
     'realtors',
@@ -130,3 +131,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Gharkhoji/static'),
 
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- Gharkhoji/Gharkhoji/urls.py
+++ Gharkhoji/Gharkhoji/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('pages/', include('pages.urls', namespace='pages')),
     path('listing/', include('listings.urls', namespace='listings')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
